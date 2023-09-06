e5485d02c5765da118892cc8642368186e915e34
Raí B. Toffoletto <rai@toffoletto.me>
2023-09-06 08:21:40 +0100

    fix: replace css link with fontsource

--- src/contexts/Theme.tsx
+++ src/contexts/Theme.tsx
@@ -1,42 +1,44 @@
 import { ThemeProvider as MuiProvider } from '@emotion/react';
-import { createTheme, CssBaseline } from '@mui/material';
+import { createTheme, CssBaseline, responsiveFontSizes } from '@mui/material';
+
+const AppTheme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: { main: '#50C' },
+      secondary: { main: '#222' },
+      error: { main: '#C22' },
+    },
+
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+        body {
+          padding: 0px 8px 16px 8px;
+          display: flex;
+          flex-direction: column;
+          overflow-x: hidden;
+          overflow-y: auto;
+          min-height: 100vh;
+          align-items: center;
+        }
+
+        #root {
+          width: 100%;
+          max-width: 1200px;
+          display: flex;
+          flex-direction: column;
+          flex-grow: 1;
+          gap: 16px;
+        }
+      `,
+      },
+    },
+  }),
+);
 
 export function ThemeProvider({ children }: IParent) {
   return (
-    <MuiProvider
-      theme={createTheme({
-        palette: {
-          primary: { main: '#50C' },
-          secondary: { main: '#222' },
-          error: { main: '#C22' },
-        },
-
-        components: {
-          MuiCssBaseline: {
-            styleOverrides: `
-              body {
-                padding: 0px 8px 16px 8px;
-                display: flex;
-                flex-direction: column;
-                overflow-x: hidden;
-                overflow-y: auto;
-                min-height: 100vh;
-                align-items: center;
-              }
-
-              #root {
-                width: 100%;
-                max-width: 1200px;
-                display: flex;
-                flex-direction: column;
-                flex-grow: 1;
-                gap: 16px;
-              }
-            `,
-          },
-        },
-      })}
-    >
+    <MuiProvider theme={AppTheme}>
       <CssBaseline />
 
       {children}
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import Router from '@views/Router';
 import { ThemeProvider } from '@contexts/Theme.tsx';
 import { UserContextProvider } from '@contexts/User.tsx';
 import sw from '@msw';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 await sw.start({
   quiet: true,
